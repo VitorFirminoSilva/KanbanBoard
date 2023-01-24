c6129e600d4e0c607d921c1c86af70f72c5c8389
VitorFirminoSilva <vitorfirmino70@gmail.com>
2023-01-24 16:14:39 -0300

    change buttons

--- js/main.js
+++ js/main.js
@@ -1,21 +1,19 @@
 import KanbanAPI from "./api/KanbanAPI.js";
+import Modal from "./view/components/Modal.js";
 import Item from "./view/Item.js";
 import Kanban from "./view/Kanban.js"
 
+
+//Kanban Main run
 new Kanban(document.querySelector(".kanban"));
 
-const addStory = document.querySelector("#add-story");
-const inputStory = document.querySelector("#story-user");
 
-
-addStory.addEventListener("click", () => {
-    const input = inputStory.value;
-
-    if(input !== ""){
-        const newItem = KanbanAPI.insertItem(1, input);
-        const column = document.querySelector(`[data-id="${1}"]`);
-        const item = new Item(newItem.id, newItem.content);
-        column.appendChild(item.elements.root);
-    }
-    inputStory.value = "";
+//Modals functions
+const modalInputNewStory = document.querySelectorAll("#modal-input-new-story");
+modalInputNewStory.forEach(element => {
+    element.addEventListener("click", e => {
+        new Modal(document.querySelector(".content"));  
+    })
 });
+
+///
