69fdcd28189072a2fc40488723624d711cc02c1e
VitorFirminoSilva <vitorfirmino70@gmail.com>
2023-03-07 14:26:31 -0300

    Sidebar Responsive Test

--- js/main.js
+++ js/main.js
@@ -11,9 +11,27 @@ new ButtonTriggerModal(document.querySelector("body"));
 //Sidebar functions
 const body = document.querySelector("body");
 const sidebar = body.querySelector(".sidebar");
-const toggle = body.querySelector(".toggle");
+const kanban = body.querySelector(".kanban");
+const toggle = body.querySelector(".toggle.angle");
+
 toggle.addEventListener("click", () => {
     sidebar.classList.toggle("close");
+    kanban.classList.toggle("close");
+});
+
+const toggleBar = body.querySelector(".toggle.bar");
+const menuBar = body.querySelector(".sidebar .menu-bar");
+const logo = body.querySelector(".sidebar header .image-text");
+
+toggleBar.addEventListener("click", () => {
+
+    toggleBar.classList.toggle("fa-bars");
+    toggleBar.classList.toggle("fa-close");
+    
+    sidebar.classList.toggle("open");
+    menuBar.classList.toggle("open");
+    logo.classList.toggle("open");
+
 });
 
 ///
